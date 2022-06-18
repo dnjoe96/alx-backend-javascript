1c7fa6a5db0483385acb3dbd51cfaeea3d9b8adb
dnjoe96 <donjoedbest@gmail.com>
2022-06-18 15:37:08 +0100

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
 import ClassRoom from "./0-classroom.js";
 
 function initializeRooms () {
-    let classes = [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34)
-    ]
+  let classes = [
+      new ClassRoom(19),
+      new ClassRoom(20),
+      new ClassRoom(34)
+  ];
 
-    return classes;
+  return classes;
 }
