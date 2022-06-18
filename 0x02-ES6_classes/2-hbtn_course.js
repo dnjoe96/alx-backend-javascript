7bab2d87f1922762593da216d784fb7eb1b06bda
dnjoe96 <donjoedbest@gmail.com>
2022-06-18 16:46:39 +0100

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -6,22 +6,28 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    if (typeof name !== 'string') throw 'Not a string';
     this._name = name;
   }
+
   get name() {
     return this._name;
   }
 
   set length(length) {
+    if (typeof length !== 'number') throw 'Not a number';
     this._length = length;
   }
+
   get length() {
     return this._length;
   }
 
   set students(students) {
+    if (typeof students !== 'object') throw 'Not an object';
     this._students = students;
   }
+
   get students() {
     return this._students;
   }
