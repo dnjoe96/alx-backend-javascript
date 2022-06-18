e07e361a81a5ac719326c67d5a9ae0fa90e7ba8d
dnjoe96 <donjoedbest@gmail.com>
2022-06-18 15:47:33 +0100

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from "./0-classroom.js";
 
-function initializeRooms () {
+export default function initializeRooms () {
   let classes = [
       new ClassRoom(19),
       new ClassRoom(20),
