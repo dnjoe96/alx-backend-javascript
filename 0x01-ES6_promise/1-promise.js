8a2d6af6f2189b903fcfb69c7769e7044fafd53a
dnjoe96 <donjoedbest@gmail.com>
2022-06-19 22:43:03 +0100

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   const promiseA = new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({status: 200, body: 'Success'});
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(Error('The fake API is not working currently'));
     }
