b5c8fcbfc42b002d5b006803d3289c56ecbd39da
dnjoe96 <donjoedbest@gmail.com>
2022-06-21 22:45:57 +0100

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 export default function cleanSet(set, startString) {
-  let str = '';
+  const arr = [];
   if (startString.length !== 0) {
     for (const one of set) {
       if (one.includes(startString, 0)) {
-        str = `${str}-${one.slice(startString.length)}`;
+        arr.push(one.slice(startString.length));
         // console.log(str);
       }
     }
+    return arr.join('-');
   }
-  return str.slice(1);
+  return '';
 }
