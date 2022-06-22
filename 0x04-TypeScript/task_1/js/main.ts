2be75af571176b539e8f2a18f86040dc6d94939a
dnjoe96 <donjoedbest@gmail.com>
2022-06-22 22:16:15 +0100

    Add a function to print Teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,6 +1,15 @@
-firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
-fullTimeEmployee(boolean) this attribute should always be defined
-yearsOfExperience(number) this attribute is optional
-location(string) this attribute should always be defined
-Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
+interface Teacher {
+  firstName: string,
+  lastName: string,
+  yearsOfExperience?: number,
+  location: string,
+  [anything]: any,
+}
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.slice(0, 1)}. ${lastName}`;
+}
