ca8486f7c3dd7a355a1d441790c195ad198e1b4e
dnjoe96 <donjoedbest@gmail.com>
2022-06-23 05:06:20 +0100

    task_2/js/main.ts

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,14 @@
+class StudentClass {
+  constructor(firstName: string, lastName: string) {
+    this.firstName: = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return; 
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
