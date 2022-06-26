876d482b60481178af096981bcf30b8876af2e72
dnjoe96 <donjoedbest@gmail.com>
2022-06-26 22:52:20 +0100

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((messages) => {
+      console.log(`${messages[0].body} ${messages[1].firstName} ${messages[1].lastName}`);
+    }).catch(() => { console.log('Signup system offline'); });
+}
