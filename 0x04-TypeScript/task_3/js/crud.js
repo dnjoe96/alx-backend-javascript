8e353fd2cdfa48616d59a057845c37928afe2b8c
dnjoe96 <donjoedbest@gmail.com>
2022-07-03 13:00:43 +0100

    Add crud.d.ts file

---
+++ 0x04-TypeScript/task_3/js/crud.js
@@ -0,0 +1,20 @@
+/**
+  * insertRow Function
+  * @param {RowElement} row
+  * @returns {RowID}
+  */
+export function insertRow(row) {
+  console.log('Insert row', row);
+  return Math.floor(Math.random() * Math.floor(1000));
+}
+
+export function deleteRow(rowId) {
+  console.log('Delete row id', rowId);
+  return;
+}
+
+export function updateRow(rowId, row) {
+  console.log(`Update row ${rowId}`, row);
+
+  return rowId;
+}
