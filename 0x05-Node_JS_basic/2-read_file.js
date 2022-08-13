23ae4656964585f573c89ee367269ec6d3db9acc
dnjoe96 <donjoedbest@gmail.com>
2022-08-13 13:27:41 +0100

    2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -25,7 +25,7 @@ function countStudents(filepath) {
 
   for (const field in fields) {
     if (field) {
-      console.log(`Number of students in ${field}: List: ${fields[field].join(', ')}`);
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
     }
   }
 }
