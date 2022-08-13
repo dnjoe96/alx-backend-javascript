a931788068f92231bdaadbe7fef170eebe0095cf
dnjoe96 <donjoedbest@gmail.com>
2022-08-13 13:19:56 +0100

    2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,22 +4,28 @@ function countStudents(filepath) {
   let data = [];
   const fields = {};
   try {
-    data = fs.readFileSync(filepath).toString().split('\n').slice(1);
+    data = fs.readFileSync(filepath, 'utf-8').split('\n').slice(1, -1);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
+  // console.log(data)
+
   console.log(`Number of students: ${data.length}`);
-  data.map((student) => student.split(','))
-    .map((student) => {
-      if (!(student[3] in fields)) {
-        fields[student[3]] = [];
-      }
-      fields[student[3]].push(student[0]);
-    });
-  for (const key in fields) {
-    if (Object.prototype.hasOwnProperty.call(fields, key)) {
-      console.log((`Number of students in ${key}: ${fields[key].length}.\
-        List: ${fields[key]}`).replace(/,/g, ', '));
+
+  for (const student of data) {
+    const field = student.split(',')[3];
+    const name = student.split(',')[0];
+    if (field in fields) {
+      fields[field].push(name);
+    } else {
+      fields[field] = [name];
+    }
+  }
+  // console.log(fields);
+
+  for (const field in fields) {
+    if (field) {
+      console.log(`Number of students in ${field}: List: ${fields[field].join(', ')}`);
     }
   }
 }
