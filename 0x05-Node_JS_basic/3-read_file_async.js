e99df5eb578f8f19f0481a1e3465497f9cec0366
dnjoe96 <donjoedbest@gmail.com>
2022-08-13 14:23:56 +0100

    3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,30 +2,33 @@ const fs = require('fs');
 
 async function countStudents(path) {
   let data;
+  const fields = {};
   try {
     data = await fs.promises.readFile(path, 'utf8');
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-  const students = data.split('\n')
-    .map((student) => student.split(','))
-    .filter((student) => student.length === 4 && student[0] !== 'firstname')
-    .map((student) => ({
-      firstName: student[0],
-      lastName: student[1],
-      age: student[2],
-      field: student[3],
-    }));
-  const csStudents = students
-    .filter((student) => student.field === 'CS')
-    .map((student) => student.firstName);
-  const sweStudents = students
-    .filter((student) => student.field === 'SWE')
-    .map((student) => student.firstName);
-  console.log(`Number of students: ${students.length}`);
-  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-  return { students, csStudents, sweStudents };
+
+  const ddata = data.split('\n').slice(1, -1);
+  // console.log(ddata);
+  console.log(`Number of students: ${ddata.length}`);
+
+  for (const student of ddata) {
+    const field = student.split(',')[3];
+    const name = student.split(',')[0];
+    if (field in fields) {
+      fields[field].push(name);
+    } else {
+      fields[field] = [name];
+    }
+  }
+  // console.log(fields);
+
+  for (const field in fields) {
+    if (field) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+  }
 }
 
 module.exports = countStudents;
