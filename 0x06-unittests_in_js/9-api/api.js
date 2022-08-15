24f82e4d20d9d9b0d5a3a25cd36b864e568c9ba4
dnjoe96 <donjoedbest@gmail.com>
2022-08-15 18:57:58 +0100

    API test

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.status(200).send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.listen(7865, () => console.log('API available on localhost port 7865'));
